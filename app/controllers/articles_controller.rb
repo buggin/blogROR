02241643f1e9fde10ccaefb545a86dda5d4e33fd
Jose Cano <josemaria.cano@gmail.com>
2014-07-08 11:50:56 +0200

    saving and listing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,23 @@ class ArticlesController < ApplicationController
   def new
   	@authors = Author.all
   end
+
+  def create
+  	@article = Article.new(article_params)
+	@article.save
+	redirect_to @article
+  end
+
+  def show
+  		@article = Article.find(params[:id])
+  end
+
+  def index
+  		@articles = Article.all
+  end
+
+  private
+	  def article_params
+	    params.require(:article).permit(:title, :body, :author_id)
+	  end
 end
